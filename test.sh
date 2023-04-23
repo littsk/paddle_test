01d6cd93d1f2e130c6919292d00da9e873adaa6c
taozewei <1214689160@qq.com>
2023-04-23 13:44:04 +0800

    add dropout

--- test.sh
+++ test.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+set -ex
+if [ ! -d "./logs" ]; then
+    mkdir -p ./logs
+    echo "Directory './logs' created."
+else
+    echo "Directory './logs' already exists."
+fi
 
 seq_lens=(4096 8192 16384 32768)
 n_heads=(16 32 64 128)
@@ -11,7 +18,11 @@ do
     do
         for test_case in "${test_cases[@]}"
         do
-            echo 1,$seq_len,$n_head
+            python ./paddle_attn_speed_test.py \
+            --yaml ./config/test_cfg.yaml \
+            --shape 1,$seq_len,$n_head,64 \
+            --test_case $test_case \
+            > result_dropout.log
         done
     done
 done
